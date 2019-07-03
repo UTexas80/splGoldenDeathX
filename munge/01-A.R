98a6a306881fcbf7928fafb842144d691af0ef4d
UTexas80 <glen.falk@utexas.edu>
2019-07-03 15:25:04 -0400

    20190703splGoldenDeathXd
    a. further dashboard cleanup.

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,21 +1,19 @@
 # Example processing script.
 
+SPL.AX.monthly <- SPL.AX[endpoints(SPL.AX, on ="months")]
+spl_month_close <- SPL.AX.monthly[,4]
+saveRDS(spl_month_close, file="spl_month_close.rds")
+
 tRet<-periodReturn(SPL.AX,period='yearly')
 SPL.AX.yearly<-data.table(date=index(tRet), coredata(tRet))
 saveRDS(SPL.AX.yearly, file="spl_yearly.rds")
 
-SPL.AX.monthly <- SPL.AX[endpoints(SPL.AX, on ="months")]
-spl_month_close <- data.table(SPL.AX.monthly[,4])
-saveRDS(spl_month_close, file="spl_month_close.rds")
-
-viz_bar_returns_by_month <- charts.PerformanceSummary(
+charts.PerformanceSummary(
     ROC(SPL.AX.monthly[,4], n = 1, type = "discrete"),
     main = "SPL Performance Summary"
 )
-saveRDS(viz_bar_returns_by_month, file="viz_bar_returns_by_month.rds")
 
-viz_bar_returns_by_yr <- plot_ly(SPL.AX.yearly, x = ~date, y = ~yearly.returns, type = 'bar', name = 'Returns by Year')
-saveRDS(viz_bar_returns_by_yr, file="viz_bar_returns_by_yr.rds")
+plot_ly(SPL.AX.yearly, x = ~date, y = ~yearly.returns, type = 'bar', name = 'Returns by Year')
 
 # VERSION HISTORY
 a01.version = "1.0.0"
