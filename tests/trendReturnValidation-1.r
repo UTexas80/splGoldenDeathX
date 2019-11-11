aba12cb4f85ea493c4b5429d63adab893c31fb22
UTexas80 <glen.falk@utexas.edu>
2019-11-10 20:02:31 -0500

    20191110splGoldenDeathXa
    a. test ideas to get column  percentage.

--- tests/trendReturnValidation-1.r
+++ tests/trendReturnValidation-1.r
@@ -24,3 +24,13 @@ head(trendSummaryGroup[indicator=='EMA'][,c(4:5)],-1)
 tail(trendSummaryGroup[indicator=='EMA'][,c(4:5)],1)
 colSums(na.omit(trendSummaryGroup)[indicator=='EMA'][,c(4:5)])
 na.omit(trendSummaryGroup)[indicator=='EMA'][,c(4:5)])/colSums(na.omit(trendSummaryGroup)[indicator=='EMA'][,c(4:5)]
+
+# Percentages per value using a data;.table
+# https://tinyurl.com/y52o5car
+# How to sum a variable by group 
+# https://tinyurl.com/yyfsfdgt
+# Aggregate using data.table     
+#  https://tinyurl.com/y2mlquvh
+# https://stackoverflow.com/questions/38530512/barplot-frequency-percentages-per-group
+
+dt[ , per := prop.table(`sum(count)`) , by = "x"]
