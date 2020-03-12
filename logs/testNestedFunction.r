f314c62d93c05263067d0a08b0b746f1a65f5bc1
UTexas80 <glen.falk@utexas.edu>
2020-03-12 16:05:27 -0400

    20200312splGoldnDeathXa
    a. mapply trial.

--- logs/testNestedFunction.r
+++ logs/testNestedFunction.r
@@ -7,6 +7,38 @@ apply(z, 1, function(x)
 str(getStrategy(nXema)$indicators)
 
 
-t(sapply(dT.indMetrics, function(a) {
-    sapply(dT.ind,        function(b) { 
-        sapply(dT.strategy,   function(c) {c})})}))
+t(
+sapply(dT.indMetrics, function(a) {
+    sapply(dT.ind, function(b) {
+        sapply(dT.strategy, function(c) { 
+            sapply(dT.name,   function(d) {a})
+            })
+        })
+    })
+)
+
+
+t(
+    sapply(z$id, function(a) {
+        sapply(z$i.id, function(b) {
+            sapply(z$i.id.1,    function(c) { 
+                sapply(dT.indMetrics, function(d) {d[2]==b && d[3]=c})
+            })
+        })
+    })
+)
+
+
+unique(dT.test1[,c(1,3,8)])
+mapply(function(x){x},c(unique(dT.test1[,c(1,3,8)])))
+
+z<- unique(dT.test1[,c(1,3,8)])
+g <- dT.test1
+y<-unique(dT.test1[,c(1,3,8)])
+setkeyv(g, keycols)
+g[y[1,]]
+
+
+#  set several columns as the key in data.table 
+# https://tinyurl.com/w2ng9pj
+setkeyv(z, c("id","i.id","i.id.1"))
